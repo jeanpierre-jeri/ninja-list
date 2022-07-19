f50bc0e4f1cf9bf18888743eb805cc98077cc176
jeanpierre-jeri <jeanpierre.webdev@gmail.com>
2022-07-19 00:51:33 -0500

    first coomit

---
+++ pages/ninjas/index.js
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import styles from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async () => {
+  const ninjas = await fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json())
+
+  return {
+    props: { ninjas },
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+
+      {ninjas.map((ninja) => (
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Ninjas
